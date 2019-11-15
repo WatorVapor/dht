a25ddbd797ac61c7c8a6ef0e4b693b7fc4a853c2
Wator Vapor <watorvapo@gmail.com>
2019-11-15 22:27:48 +0900

    dht

--- test/peer.global.js
+++ test/peer.global.js
@@ -1,8 +1,1 @@
-'use strict';
-const config = require('./node1.config.js');
-console.log(':: config=<',config,'>');
-const DHT = require('../dht.js');
-const dht = new DHT(config);
-//console.log(':: dht=<',dht,'>');
-const peer = dht.peerInfo();
-console.log(':: peer=<',peer,'>');
+peer.node.js
