86d3c783837ff3a800f70c10449b04abfb5caa56
Wator Vapor <watorvapo@gmail.com>
2019-12-03 22:32:59 +0900

    dht

--- cli/wdht.js
+++ cli/wdht.js
@@ -37,6 +37,7 @@ console.log(':: peer=<',peer,'>');
 const appendData = ()=> {
   dht.append('汉语','https://zh.wikipedia.org/wiki/汉语');
   dht.append('航母','https://www.6parknews.com/newspark/view.php?app=news&act=view&nid=386668');
+  dht.append('航母','https://zh.wikipedia.org/wiki/%E8%88%AA%E7%A9%BA%E6%AF%8D%E8%88%B0');
   dht.append('海试','https://www.6parknews.com/newspark/view.php?app=news&act=view&nid=386668');
 };
 
--- src/resource.storage.js
+++ src/resource.storage.js
@@ -2,7 +2,6 @@
 const fs = require('fs');
 const path = require('path');
 const jsrsasign = require('jsrsasign');
-//const bs58 = require('bs58');
 const RIPEMD160 = require('ripemd160');
 const base32 = require('base32');
 const PeerMachine = require('./peer.machine.js');
@@ -13,38 +12,50 @@ const PeerMachine = require('./peer.machine.js');
 class ResourceStorage {
   constructor(config) {
     console.log('ResourceStorage::constructor: config.reps=<',config.reps,'>');
-    this._path = config.reps.data + '/ldfs';
-    if (!fs.existsSync(this._path)) {
-      fs.mkdirSync(this._path,{ recursive: true });
+    this._pathKey = config.reps.data + '/keyspace';
+    if (!fs.existsSync(this._pathKey)) {
+      fs.mkdirSync(this._pathKey,{ recursive: true });
     }
-    console.log('ResourceStorage::constructor: config=<',config,'>');
+    this._pathContent = config.reps.data + '/dataspace';
+    if (!fs.existsSync(this._pathContent)) {
+      fs.mkdirSync(this._pathContent,{ recursive: true });
+    }    console.log('ResourceStorage::constructor: config=<',config,'>');
     this.machine_ = new PeerMachine(config);
     const host = this.machine_.readMachienIp();
     const port = config.listen.data.port;
     this._uri = `https://[${host}]:${port}`;
   }
+  
   append(key,content) {
-    let keyAddress = this.getContentAddress_(key);
-    console.log('ResourceStorage::append: keyAddress=<',keyAddress,'>');
-    let keyPath = this.getPath4Address_(keyAddress);
+    const keyAddress = this.getAddress_(key);
+    //console.log('ResourceStorage::append: keyAddress=<',keyAddress,'>');
+    const keyPath = this.getPath4KeyAddress_(keyAddress);
     console.log('ResourceStorage::append: keyPath=<',keyPath,'>');
     if (!fs.existsSync(keyPath)) {
       fs.mkdirSync(keyPath,{ recursive: true });
     }
-    const contentAddress = this.getContentAddress_(content);
-    const contentPath = keyPath + '/' + contentAddress;
-    console.log('ResourceStorage::append: contentPath=<',contentPath,'>');
-    fs.writeFileSync(contentPath,content);
+    const contentAddress = this.getAddress_(content);
+    const contentPlacePath = keyPath + '/' + contentAddress;
+    //console.log('ResourceStorage::append: contentPlacePath=<',contentPlacePath,'>');
+    fs.writeFileSync(contentPlacePath,'');
+    
+    const contentPathFlat = this.getPath4ContentAddress_(contentAddress);
+    const contentPathFlatDir = path.dirname(contentPathFlat);
+    //console.log('ResourceStorage::append: contentPathFlatDir=<',contentPathFlatDir,'>');
+    if (!fs.existsSync(contentPathFlatDir)) {
+      fs.mkdirSync(contentPathFlatDir,{ recursive: true });
+    }
+    fs.writeFileSync(contentPathFlat,content);
+    
     return {address:keyAddress,uri:this.getURI4Address_(keyAddress)};
   }
+  
   fetch(keyAddress,start,count) {
     //console.log('ResourceStorage::fetch: keyAddress=<',keyAddress,'>');
-    let keyPath = this.getPath4Address_(keyAddress);
+    const keyPath = this.getPath4KeyAddress_(keyAddress);
     //console.log('ResourceStorage::fetch: keyPath=<',keyPath,'>');
     if(fs.existsSync(keyPath)) {
       //console.log('ResourceStorage::fetch: keyPath=<',keyPath,'>');
-      const source = fs.readdirSync(keyPath, { withFileTypes: true });
-      //console.log('ResourceStorage::fetch: source=<',source,'>');
       const stat = fs.lstatSync(keyPath);
       //console.log('ResourceStorage::fetch: stat=<',stat,'>');
       if(stat.isDirectory()) {
@@ -53,22 +64,41 @@ class ResourceStorage {
       if(stat.isFile()) {
         return this.fetchFile_(keyPath);
       }
+    } else {
+      const contentPath = this.getPath4ContentAddress_(keyAddress);
+      if(fs.existsSync(contentPath)) {
+        //console.log('ResourceStorage::fetch: contentPath=<',contentPath,'>');
+        const stat = fs.lstatSync(contentPath);
+        //console.log('ResourceStorage::fetch: stat=<',stat,'>');
+        if(stat.isFile()) {
+          return this.fetchFile_(contentPath);
+        }
+      }
     }
     return null;
  }
 
-  getContentAddress_(resourceKey) {
+
+  getAddress_(resourceKey) {
     const resourceRipemd = new RIPEMD160().update(resourceKey).digest('hex');
     const resourceBuffer = Buffer.from(resourceRipemd,'hex');
     return base32.encode(resourceBuffer);
     return 
   }
-  getPath4Address_(address) {
-    let pathAddress = this._path;
+  getPath4KeyAddress_(address) {
+    let pathAddress = this._pathKey;
     pathAddress += '/' + address.substring(0,3);
+    pathAddress += '/' + address.substring(3,6);
     pathAddress += '/' + address;
     return pathAddress;
   }
+  getPath4ContentAddress_(address) {
+    let pathAddress = this._pathContent;
+    pathAddress += '/' + address.substring(0,3);
+    pathAddress += '/' + address.substring(3,6);
+    pathAddress += '/' + address;
+    return pathAddress;
+  }  
   getURI4Address_(address) {
     let uriAddress = this._uri;
     uriAddress += '/' + address;
@@ -76,19 +106,22 @@ class ResourceStorage {
   }
   
   fetchDir_(dirPath,start,count) {
-    console.log('ResourceStorage::fetchDir_: dirPath=<',dirPath,'>');
+    //console.log('ResourceStorage::fetchDir_: dirPath=<',dirPath,'>');
     const files = fs.readdirSync(dirPath);
-    console.log('ResourceStorage::fetchDir_: files=<',files,'>');
-    const rangeS = start -1;
+    //console.log('ResourceStorage::fetchDir_: files=<',files,'>');
+    const rangeS = start;
     let rangeE = rangeS+count;
     if(rangeE > files.length) {
       rangeE = files.length
     }
+    //console.log('ResourceStorage::fetchDir_: rangeS=<',rangeS,'>');
+    //console.log('ResourceStorage::fetchDir_: rangeE=<',rangeE,'>');
     return files.slice(rangeS,rangeE);
   }
   
   fetchFile_(filePath) {
-    console.log('ResourceStorage::fetchFile_: filePath=<',filePath,'>');
+    //console.log('ResourceStorage::fetchFile_: filePath=<',filePath,'>');
+    return fs.readFileSync(filePath).toString('utf-8');
   }
 
 }
