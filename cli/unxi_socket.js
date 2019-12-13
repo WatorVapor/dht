9512672382158c091bc030c6ef37a07c78abb79b
Wator Vapor <watorvapor@gmail.com>
2019-12-13 15:37:56 +0900

    Update unxi_socket.js

--- cli/unxi_socket.js
+++ cli/unxi_socket.js
@@ -9,16 +9,16 @@ dht.peerInfo((peer)=>{
 
 const appendData = ()=> {
   dht.append('汉语','https://zh.wikipedia.org/wiki/汉语',(info) => {
-    console.log('dht.append:: info=<',info,'>');
+    console.log('dht.append:: info.store=<',info.store,'>');
   });  
   dht.append('航母','https://www.6parknews.com/newspark/view.php?app=news&act=view&nid=386668',(info) => {
-    console.log('dht.append:: info=<',info,'>');
+    console.log('dht.append:: info.store=<',info.store,'>');
   });
   dht.append('航母','https://zh.wikipedia.org/wiki/%E8%88%AA%E7%A9%BA%E6%AF%8D%E8%88%B0',(info) => {
-    console.log('dht.append:: info=<',info,'>');
+    console.log('dht.append:: info.store=<',info.store,'>');
   });
   dht.append('海试','https://www.6parknews.com/newspark/view.php?app=news&act=view&nid=386668',(info)=> {
-    console.log('dht.append:: info=<',info,'>');
+    console.log('dht.append:: info.store=<',info.store,'>');
   });
 };
 
