d8040e359ef117326f388c4a047768f9cce14486
Wator Vapor <watovapor@gmail.com>
2020-01-07 14:24:28 +0000

    ermu

--- api/DHTRedis.js
+++ api/DHTRedis.js
@@ -1,18 +1,19 @@
 'use strict';
-const net = require('net');
+const redis = require('redis');
+const redisOption = {
+  path:'/dev/shm/dht.ermu.api.redis.sock'
+};
 const RIPEMD160 = require('ripemd160');
 const base32 = require("base32.js");
-const API_DOMAIN_PATH = '/dev/shm/dht_ermu_api_unix_dgram.sock';
-const StreamJson = require('./StreamJson.js');
 const bs32Option = { type: "crockford", lc: true };
 const https = require('https');
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
-class DHTUnixSocket {
+class DHTRedis {
   constructor() {
-    console.log('DHTUnixSocket::constructor');
-    this.client_ = net.createConnection(API_DOMAIN_PATH);
-    this.client_.setNoDelay();
+    console.log('DHTRedis::constructor');
+    this.client_ = redis.createClient(redisOption);
+    
     const self = this;
     this.client_.on('error', (err) => {
       self.onError_(err);
@@ -28,14 +29,14 @@ class DHTUnixSocket {
     this.sjson_ = new StreamJson();
   }
   peerInfo(cb) {
-    console.log('DHTUnixSocket::peerInfo');
+    console.log('DHTRedis::peerInfo');
     const msg = {peerInfo:'get'};
     const cbTag = this.writeData_(msg);
     this.cb_[cbTag] = cb;
   }
   append(key,data,cb) {
-    console.log('DHTUnixSocket::append key=<',key,'>');
-    console.log('DHTUnixSocket::append data=<',data,'>');
+    console.log('DHTRedis::append key=<',key,'>');
+    console.log('DHTRedis::append data=<',data,'>');
     const msg = {
       store:'append',
       key:key,
@@ -45,7 +46,7 @@ class DHTUnixSocket {
     this.cb_[cbTag] = cb;
   }
   fetch4KeyWord(keyWord,cb) {
-    console.log('DHTUnixSocket::fetch4KeyWord keyWord=<',keyWord,'>');
+    console.log('DHTRedis::fetch4KeyWord keyWord=<',keyWord,'>');
     const msg = {
       fetch:'keyWord',
       keyWord:keyWord
@@ -55,24 +56,24 @@ class DHTUnixSocket {
   }
   
   onError_(err) {
-    console.log('DHTUnixSocket::onError_ err=<',err,'>');
+    console.log('DHTRedis::onError_ err=<',err,'>');
   }
   onMsg_(msg) {
-    //console.log('DHTUnixSocket::onMsg_ msg=<',msg.toString('utf-8'),'>');
+    //console.log('DHTRedis::onMsg_ msg=<',msg.toString('utf-8'),'>');
     const jMsgArray = this.sjson_.parse(msg.toString());
-    //console.log('DHTUnixSocket::onMsg_ jMsgArray=<',jMsgArray,'>');
+    //console.log('DHTRedis::onMsg_ jMsgArray=<',jMsgArray,'>');
     for(const jMsg of jMsgArray ) {
-      //console.log('DHTUnixSocket::onMsg_ jMsg=<',jMsg,'>');
+      //console.log('DHTRedis::onMsg_ jMsg=<',jMsg,'>');
       if(jMsg) {
         if(jMsg.peerInfo) {
           this.onPeerInfo_(jMsg);
         } else if(jMsg.fetchResp) {
           this.onFetchResp_(jMsg);
         } else {
-          console.log('DHTUnixSocket::onMsg_ jMsg=<',jMsg,'>');          
+          console.log('DHTRedis::onMsg_ jMsg=<',jMsg,'>');          
         }
       } else {
-        console.log('DHTUnixSocket::onMsg_ jMsg=<',jMsg,'>');
+        console.log('DHTRedis::onMsg_ jMsg=<',jMsg,'>');
       }
     }
   }
@@ -102,48 +103,48 @@ class DHTUnixSocket {
     if( typeof this.cb_[jMsg.cb] === 'function') {
       this.cb_[jMsg.cb](jMsg.peerInfo);
     } else {
-      console.log('DHTUnixSocket::onPeerInfo_ jMsg=<',jMsg,'>');
-      console.log('DHTUnixSocket::onPeerInfo_ this.cb_=<',this.cb_,'>');
+      console.log('DHTRedis::onPeerInfo_ jMsg=<',jMsg,'>');
+      console.log('DHTRedis::onPeerInfo_ this.cb_=<',this.cb_,'>');
     }
   }
 
   onFetchResp_(jMsg) {
-    //console.log('DHTUnixSocket::onFetchResp_ jMsg=<',jMsg,'>');
+    //console.log('DHTRedis::onFetchResp_ jMsg=<',jMsg,'>');
     const address = jMsg.address;
     for(const keyAddress in jMsg.fetchResp) {
-      //console.log('DHTUnixSocket::onFetchResp_:: keyAddress=<',keyAddress,'>');
+      //console.log('DHTRedis::onFetchResp_:: keyAddress=<',keyAddress,'>');
       const uri = jMsg.fetchResp[keyAddress] + '/' + address;
-      //console.log('DHTUnixSocket::onFetchResp_:: uri=<',uri,'>');
+      //console.log('DHTRedis::onFetchResp_:: uri=<',uri,'>');
       const self = this;
       this.requestURI_(uri,(data) => {
         try {
           const jData = JSON.parse(data);
-          //console.log('DHTUnixSocket::onFetchResp_:: jData=<',jData,'>');
+          //console.log('DHTRedis::onFetchResp_:: jData=<',jData,'>');
           self.onFetchResource_(jData,jMsg.fetchResp[keyAddress],jMsg.cb,keyAddress);
         }catch(e) {
-          console.log('DHTUnixSocket::onFetchResp_:: e=<',e,'>');
+          console.log('DHTRedis::onFetchResp_:: e=<',e,'>');
         }
       });
     }
   }
   onFetchResource_(jData,url,cb,keyAddress) {
-    //console.log('DHTUnixSocket::onFetchResource_:: jData=<',jData,'>');
-    //console.log('DHTUnixSocket::onFetchResource_:: url=<',url,'>');
-    //console.log('DHTUnixSocket::onFetchResource_:: cb=<',cb,'>');
+    //console.log('DHTRedis::onFetchResource_:: jData=<',jData,'>');
+    //console.log('DHTRedis::onFetchResource_:: url=<',url,'>');
+    //console.log('DHTRedis::onFetchResource_:: cb=<',cb,'>');
     const self = this;
     for(const address of jData) {
-      //console.log('DHTUnixSocket::onFetchResource_:: address=<',address,'>');
+      //console.log('DHTRedis::onFetchResource_:: address=<',address,'>');
       const uri = url + '/' + address;
-      //console.log('DHTUnixSocket::onFetchResource_:: uri=<',uri,'>');
+      //console.log('DHTRedis::onFetchResource_:: uri=<',uri,'>');
       this.requestURI_(uri,(data)=> {
-        //console.log('DHTUnixSocket::onFetchResource_:: data=<',data,'>');
+        //console.log('DHTRedis::onFetchResource_:: data=<',data,'>');
         self.onFetchResourceData_(data,address,keyAddress,cb);
       });
     }
   }
   onFetchResourceData_(data,address,keyAddress,cb) {
-    //console.log('DHTUnixSocket::onFetchResourceData_:: data=<',data,'>');
-    //console.log('DHTUnixSocket::onFetchResourceData_:: cb=<',cb,'>');
+    //console.log('DHTRedis::onFetchResourceData_:: data=<',data,'>');
+    //console.log('DHTRedis::onFetchResourceData_:: cb=<',cb,'>');
     if( typeof this.cb_[cb] === 'function') {
       const resource = {
         data:data,
@@ -152,8 +153,8 @@ class DHTUnixSocket {
       }
       this.cb_[cb](resource);
     } else {
-      console.log('DHTUnixSocket::onFetchResp_ cb=<',cb,'>');
-      console.log('DHTUnixSocket::onFetchResp_ this.cb_=<',this.cb_,'>');
+      console.log('DHTRedis::onFetchResp_ cb=<',cb,'>');
+      console.log('DHTRedis::onFetchResp_ this.cb_=<',this.cb_,'>');
     }
   }
   
@@ -161,18 +162,18 @@ class DHTUnixSocket {
     const request = https.get(uri, (res) => {
       let data = '';
       res.on('data', (d) => {
-        //console.log('DHTUnixSocket::requestURI_:: d=<',d,'>');
+        //console.log('DHTRedis::requestURI_:: d=<',d,'>');
         data += d.toString('utf-8');
       });
       res.on('end', () => {
-        //console.log('DHTUnixSocket::requestURI_:: data=<',data,'>');
+        //console.log('DHTRedis::requestURI_:: data=<',data,'>');
         cb(data);
       });
     });
     request.on('error', (e) => {
-      console.log('DHTUnixSocket::requestURI_:: e=<',e,'>');
+      console.log('DHTRedis::requestURI_:: e=<',e,'>');
     })    
   }
 }
 
-module.exports = DHTUnixSocket;
+module.exports = DHTRedis;
