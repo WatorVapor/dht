9c4182d32d090ddf7f5896428c2464794ded8bde
Wator Vapor <watovapor@gmail.com>
2020-01-08 14:00:48 +0000

    ermu

---
+++ cli/redis.daemon_proxy.js
@@ -0,0 +1,39 @@
+'use strict';
+const path = require('path');
+//console.log(':: __filename=<',__filename,'>');
+const dhtPath = '/storage/dhtfs/cluster/' + path.parse(__filename).name + '/peerstore';
+const dataPath = '/storage/dhtfs/cluster/' + path.parse(__filename).name + '/datastore';
+const config = {
+  listen:{
+    ctrl:{
+      port:8890
+    },
+    data:{
+      port:8891
+    }
+  },
+  entrance:[
+    {
+      host:'ermu3.wator.xyz',
+      port:8890
+    },
+    {
+      host:'ermu4.wator.xyz',
+      port:8890
+    }
+  ],
+  reps: {
+    dht:dhtPath,
+    data:dataPath
+  },
+  storage:false
+};
+//console.log(':: config=<',config,'>');
+const DHT = require('../src/dht.js');
+const dht = new DHT(config);
+//console.log(':: dht=<',dht,'>');
+const peer = dht.peerInfo();
+console.log(':: peer=<',peer,'>');
+
+const DaemonRedis = require('../api/DaemonRedis.js');
+const daemon = new DaemonRedis(dht);
