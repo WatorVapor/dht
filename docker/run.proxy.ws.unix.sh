c625d30aadf82e570c49e70738cb9dc1e5e79f42
Wator Vapor <watorvapo@gmail.com>
2020-08-25 09:32:16 +0900

    modify words

--- docker/run.proxy.ws.unix.sh
+++ docker/run.proxy.ws.unix.sh
@@ -3,20 +3,19 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 PARENT_DiR=$(dirname $SCRIPT_DIR)
 echo "SCRIPT_DIR:=${SCRIPT_DIR}"
 echo "PARENT_DiR:=${PARENT_DiR}"
-DOCKER_MAME=ermu.indexer
+DOCKER_MAME=ermu.proxy.ws.unix
 docker stop ${DOCKER_MAME}
 docker rm ${DOCKER_MAME}
 docker run -d \
   -v /etc/group:/etc/group:ro \
   -v /etc/passwd:/etc/passwd:ro \
-  -v /storage/dhtfs/cluster:/storage/dhtfs/cluster \
   -v /dev/shm/:/dev/shm/ \
   -v ${PARENT_DiR}:${PARENT_DiR} \
   -u $(id -u $USER):$(id -g $USER) \
   -w ${PARENT_DiR} \
   --net host \
-  --memory=512M \
-  --cpu-shares=1024 \
+  --memory=32M \
+  --cpu-shares=128 \
   --name "${DOCKER_MAME}" \
   --restart=always \
-  node:lts node ./indexer/app.js >/dev/null
+  node:lts node ./wsproxy/ws.ermu.unix.js
---
+++ wsproxy/ws.ermu.unix.js
@@ -0,0 +1,96 @@
+const http = require('http');
+const WebSocket = require('ws');
+const fs = require('fs');
+const { execSync } = require('child_process')
+
+const crypto = require('crypto');
+
+const socketPath = '/tmp/wator/ermu.wator.xyz' ;
+fs.existsSync(socketPath) && fs.unlinkSync(socketPath);
+const hs = http.createServer();
+hs.listen(socketPath, () => {
+  console.log('hs.listen:socketPath=<',socketPath,'>');
+  execSync(`chmod 777 ${socketPath}`);
+});
+
+const channelWS2DHT = 'enum.www.search.ws2dht';
+const channelDHT2WS = 'enum.www.search.dht2ws';
+const wss = new WebSocket.Server({server: hs});
+
+wss.on('connection', (ws) => {
+  onConnected(ws);
+});
+
+
+wss.on('connection', (ws) => {
+  onConnected(ws);
+});
+
+const onConnected = (ws) => {
+  ws.on('message', (message) => {
+    onWSMsg(message,ws);
+  });
+  ws.on('error', (evt) => {
+    console.log('error: evt=<', evt,'>');
+  });
+  ws.isAlive = true;
+  ws.on('pong', heartBeatWS);  
+}
+const gCallBack = {};
+const onWSMsg = (message,ws) => {
+  console.log('onWSMsg: message=<', message,'>');
+  try {
+    const jsonMsg = JSON.parse(message);
+    if(jsonMsg) {
+      const buf = crypto.randomBytes(16);
+      jsonMsg.cbtag = buf.toString('hex');
+      pubRedis.publish(channelWS2DHT,JSON.stringify(jsonMsg));
+      gCallBack[jsonMsg.cbtag] = ws;
+    }
+  } catch(e) {
+    
+  }
+};
+
+const redis = require('redis');
+const redisOption = {
+  path:'/dev/shm/dht.ermu.api.redis.sock'
+};
+
+const subRedis = redis.createClient(redisOption);
+const pubRedis  = redis.createClient(redisOption);
+subRedis.on('message', (channel, message) => {
+  onRedisMsg(channel, message);
+});
+subRedis.subscribe(channelDHT2WS);
+
+const onRedisMsg = (channel, message) => {
+  try {
+    const jsonMsg = JSON.parse(message);
+    if(jsonMsg && jsonMsg.cbtag) {
+      const client = gCallBack[jsonMsg.cbtag];
+      if(client) {
+        client.send(JSON.stringify(jsonMsg));
+      }
+    }
+  } catch(e) {
+    
+  }
+}
+
+
+const noop = () => {};
+const heartBeatWS = () => {
+  console.log('heartBeatWS::this=<',this,'>');
+  this.isAlive = true;
+}
+const doPingWS = () => {
+  wss.clients.forEach((ws) => {
+    ws.isAlive = false;
+    ws.ping(noop);
+  });  
+};
+const interval = setInterval(doPingWS, 10000);
+
+
+
